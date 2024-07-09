045ce59ce3f6d655c8a048e1acf57338f732f0ce
xlight05 <anjanasupun05@gmail.com>
2024-07-09 18:47:43 +0530

    Update code for the first impl

--- main.py
+++ main.py
@@ -16,31 +16,27 @@ app.add_middleware(
 
 async def code_generator():
     yield f"event: resp_start\ndata: {""}\n\n"
-    yield f"event: libraries\ndata: {"[\"ballerina/io\",\"ballerinax/googleapis.sheets\"]"}\n\n"
-    yield f"event: functions\ndata: {'[{"name":"ballerinax/googleapis.sheets","clients":[{"name":"Client","functions":[{"name":"createSpreadsheet"},{"name":"openSpreadsheetById"},{"name":"appendValue"}]}]},{"name":"ballerina/io","functions":[{"name":"fileReadCsv"}]}]'}\n\n"
+    libs = {"libs": ["ballerina/io","ballerinax/googleapis.sheets"]}
+    yield f"event: libraries\ndata: {json.dumps(libs)}\n\n"
+    funcs = {"funcs":[{"name":"ballerinax/googleapis.sheets","clients":[{"name":"Client","functions":[{"name":"createSpreadsheet"},{"name":"openSpreadsheetById"},{"name":"appendValue"}]}]},{"name":"ballerina/io","functions":[{"name":"fileReadCsv"}]}]}
+    yield f"event: functions\ndata: {json.dumps(funcs)}\n\n"
     yield f"event: message_start\ndata: {""}\n\n"
-    with open("first.txt", 'r') as file:
+    with open("explain.txt", 'r') as file:
         while True:
             char = file.read(5)
             if not char:
                 break
             data = {"delta":{"type":"text_delta","text":char}}
             yield f"event: content_delta\ndata: {json.dumps(data)}\n\n"
-    yield f"event: message_stop\ndata: {""}\n\n"
-   
-    yield f"event: compile_started\ndata: {""}\n\n"
-    yield f"event: diagnostics\ndata: {"[\"ERROR [balSource17696242700046434752.bal:(30:40,30:56)] incompatible types: expected \'(string[][]|map<anydata>[])\', found \'(int|string|decimal)[][]\'\"]"}\n\n"
-    
-    yield f"event: message_start\ndata: {""}\n\n"
+    data = {"delta":{"type":"text_delta","text":"```bal"}}
+    yield f"event: content_delta\ndata: {json.dumps(data)}\n\n"
+    with open('first_code.txt', 'r') as file:
+        content = file.read()
+        data = {"delta":{"type":"text_delta","text":content}}
+        yield f"event: content_delta\ndata: {json.dumps(data)}\n\n"
 
-    with open("second.txt", 'r') as file:
-        while True:
-            char = file.read(5)
-            if not char:
-                break
-            data = {"delta":{"type":"text_delta","text":char}}
-            yield f"event: content_delta\ndata: {json.dumps(data)}\n\n"
-
+    data = {"delta":{"type":"text_delta","text":"```"}}
+    yield f"event: content_delta\ndata: {json.dumps(data)}\n\n"
     yield f"event: message_stop\ndata: {""}\n\n"
     yield f"event: resp_stop\ndata: {""}\n\n"
 
